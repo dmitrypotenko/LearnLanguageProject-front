818c0d6302726af0a0467fdc1e9194561abde91c
Dzmitry_Potenko <Dzmitry_Potenko@epam.com>
2020-02-08 13:52:56 +0300

    sticky menu

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,6 +4,9 @@ import {LessonData, LessonService} from '../lesson.service';
 import {ActivatedRoute} from '@angular/router';
 import {TestData, TestService} from '../test.service';
 import {StepSwitcherService} from '../step-switcher.service';
+import {MediaMatcher} from '@angular/cdk/layout';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-course',
@@ -19,10 +22,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   private cd: ChangeDetectorRef;
   private _testService: TestService;
+  private sidenav: any;
 
   constructor(courseService: CourseService, lessonService: LessonService, route: ActivatedRoute,
               cd: ChangeDetectorRef,
-              testService: TestService) {
+              testService: TestService,
+              private mediaMatcher: MediaMatcher) {
     this.cd = cd;
     this.courseService = courseService;
     this.lessonService = lessonService;
@@ -44,6 +49,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
+    this.sidenav = jQuery('#sidenavStep');
     if (this.tryToUpdate()) {
       this.cd.detectChanges();
     }
@@ -81,9 +87,42 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
 // When the user clicks on the button, scroll to the top of the document
+
   topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
+  sidenavmode(): boolean {
+    if (this.mediaMatcher.matchMedia('(max-width: 800px)').matches) {
+      return false;
+    }
+    return true;
+  }
+
+  private menu: any;
+
+  menuHeight(): number {
+    let visibleHeight = jQuery('#mainMenu').outerHeight(true) - jQuery(window).scrollTop();
+    if (visibleHeight > 0) {
+      return visibleHeight;
+    }
+    return 0;
+  }
+
+  sideWidth(): number {
+    if (this.sidenav == null) {
+      return 0;
+    }
+    return this.sidenav.width() + this.sidenav.position().left;
+  }
+
+  changeToggleButton() {
+    /*  let toggleBtn = jQuery('#toggleBtn');
+      if (this.opened) {
+        toggleBtn.css({left: this.sidenav.width().toString()});
+      } else {
+        toggleBtn.css({left: '0'});
+      }*/
+  }
 }
